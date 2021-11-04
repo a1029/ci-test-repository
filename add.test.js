8d90ac8e8084585e6bd581ed30ffe3d9ceaa350d
J065_김한빈 <rttr1023@gmail.com>
2021-11-05 02:10:34 +0900

    sub 함수 구현

--- add.test.js
+++ add.test.js
@@ -1,10 +1,22 @@
-const { test, expect } = require("@jest/globals");
-const { add } = require("./add");
+const { test, expect, describe, it } = require("@jest/globals");
+const { add, sub } = require("./add");
 
-test("2+5=7", () => {
-  expect(add(2, 5)).toBe(7);
+describe("add test", () => {
+  test("2+5=7", () => {
+    expect(add(2, 5)).toBe(7);
+  });
+
+  test("10+5=15", () => {
+    expect(add(10, 5)).toBe(15);
+  });
 });
 
-test("2+5=7", () => {
-  expect(add(2, 5)).toBe(7);
+describe("sub test", () => {
+  it("2-5=-3", () => {
+    expect(sub(2, 5)).toBe(-3);
+  });
+
+  it("50-30=20", () => {
+    expect(sub(50, 30)).toBe(20);
+  });
 });
