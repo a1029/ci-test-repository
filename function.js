61f8a5b72fe6aed21d6a0b652252b7da4e0baf7f
J065_김한빈 <rttr1023@gmail.com>
2021-11-19 11:04:44 +0900

    테스트 성공

--- function.js
+++ function.js
@@ -6,11 +6,7 @@ function sub(a, b) {
   return a - b;
 }
 
-function sayHello(name) {
-  return "hello " + name;
-}
 module.exports = {
   add,
   sub,
-  sayHello,
 };
--- function.test.js
+++ function.test.js
@@ -1,26 +1,14 @@
 const { test, expect, describe, it } = require("@jest/globals");
-const { add, sub, sayHello } = require("./function");
+const { add, sub } = require("./function");
 
 describe("add test", () => {
-  test("2+5=7", () => {
-    expect(add(2, 5)).toBe(7);
-  });
-
-  test("10+5=15", () => {
-    expect(add(10, 5)).toBe(15);
+  test("5 + 2 = 7", () => {
+    expect(add(5, 2)).toBe(7);
   });
 });
 
 describe("sub test", () => {
-  it("2-5=-3", () => {
-    expect(sub(2, 5)).toBe(-3);
+  it("5 - 2 = 3", () => {
+    expect(sub(5, 2)).toBe(-3);
   });
-
-  it("50-30=20", () => {
-    expect(sub(50, 30)).toBe(20);
-  });
-});
-
-test("hello boostcamp", () => {
-  expect(sayHello("boostcamp")).toBe("hello boostcampp");
 });
